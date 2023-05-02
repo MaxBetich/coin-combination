6c284a5c310f3d901e1c8405a318d363573813c9
Max Betich <betichm@gmail.com>
2023-05-02 11:05:48 -0700

    add error handling to program

--- Coin/Program.cs
+++ Coin/Program.cs
@@ -7,12 +7,21 @@ namespace Coin
   {
     static void Main()
     {
-      Console.WriteLine("Enter an amount of cents to convert to the minimum number of coins:");
-      string userInput = Console.ReadLine();
-      int number = int.Parse(userInput);
-      Counter newCounter = new Counter();
-      string outputString = newCounter.CoinReturn(number);
-      Console.WriteLine(outputString);
+      try
+      {
+        Console.WriteLine("Enter an amount of cents to convert to the minimum number of coins:");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        Counter newCounter = new Counter();
+        string outputString = newCounter.CoinReturn(number);
+        Console.WriteLine(outputString);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Message = {0}", ex.Message);
+        Console.WriteLine("Please enter a whole number to continue");
+        Main();
+      }
     }
   }
 }
