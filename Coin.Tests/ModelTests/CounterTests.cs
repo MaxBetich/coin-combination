380177d88adf1f40acac1a01d4481ec2a8a58133
Max Betich <betichm@gmail.com>
2023-05-02 09:40:24 -0700

    add new CoinReturn test

--- Coin.Tests/ModelTests/CounterTests.cs
+++ Coin.Tests/ModelTests/CounterTests.cs
@@ -6,13 +6,18 @@ namespace Coin.Tests
   [TestClass]
   public class CounterTests
   {
+    Counter newCounter = new Counter();
     [TestMethod]
     public void CounterConstructor_CreatesInstanceOfCounter_Counter()
     {
-      Counter newCounter = new Counter();
       Assert.AreEqual(typeof(Counter), newCounter.GetType());
     }
 
-    
+    [TestMethod]
+    public void CoinReturn_ReturnsStringOfValues_String()
+    {
+      string coinReturn = newCounter.CoinReturn(4);
+      Assert.AreEqual("4 pennies", coinReturn);
+    }
   }
 }
