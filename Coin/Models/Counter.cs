cdc262082ebdae66f6c6c55c2d36d73efb2fc080
Max Betich <betichm@gmail.com>
2023-05-02 11:27:24 -0700

    update CoinReturn2 method

--- Coin/Models/Counter.cs
+++ Coin/Models/Counter.cs
@@ -16,8 +16,13 @@ namespace Coin.Models
 
     public string CoinReturn2(int inputValue)
     {
-      int pennies = inputValue;
-      string output = $"0 quarter(s) and 0 dime(s) and 0 nickel(s) and {pennies} pennies";
+      int nickels = (int)(inputValue/5);
+      if (nickels > 2)
+      {
+        nickels = 2;
+      }
+      int pennies = inputValue -(nickels*5);
+      string output = $"0 quarter(s) and 0 dime(s) and {nickels} nickel(s) and {pennies} pennies";
       return output;
     }
   }
